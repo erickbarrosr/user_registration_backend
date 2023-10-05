dd13fe7910e2e8c9d75e84e9a135aff7f4686def
Erick Barros <erickbarrosrezende@gmail.com>
2023-10-05 20:48:45 -0300

    feat: started to create for the user controller

---
+++ controllers/userController.js
@@ -0,0 +1,11 @@
+const UserModel = require("../models/User");
+
+const userController = {
+  create: async (req, res) => {},
+  index: async (req, res) => {},
+  show: async (req, res) => {},
+  update: async (req, res) => {},
+  destroy: async (req, res) => {},
+};
+
+module.exports = userController;
