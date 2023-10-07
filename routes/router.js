6dcf753039411da8b81376e1e8fbdc586a09f239
Erick Barros <erickbarrosrezende@gmail.com>
2023-10-07 13:26:01 -0300

    feat: created router for user register

--- routes/router.js
+++ routes/router.js
@@ -1,7 +1,11 @@
 const router = require("express").Router();
+const usersRouter = require("./users");
 
+// Public Route
 router.route("/").get((req, res) => {
   res.status(200).json({ message: "Hello World!" });
 });
+
+router.use("/", usersRouter);
 
 module.exports = router;
