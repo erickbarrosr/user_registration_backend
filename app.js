b96b8512d9a11574afdf3d057ecf982b7fb17fb3
Erick Barros <erickbarrosrezende@gmail.com>
2024-05-07 22:51:00 -0300

    refactor(app.js): changes and updates for es6

--- app.js
+++ app.js
@@ -1,14 +1,37 @@
-const express = require("express");
-const cors = require("cors");
-const routes = require("./routes/router");
-const database = require("./db");
+import express from "express";
+import cors from "cors";
+import database from "./database";
 
-const app = express();
-database();
+class App {
+  constructor() {
+    this.app = express();
+    this.db = database();
+    this.middlewares();
+    this.routes();
+  }
 
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
-app.use(cors());
-app.use("/api", routes);
+  middlewares() {
+    this.app.use(express.urlencoded({ extended: true }));
+    this.app.use(express.json());
+    this.app.use(cors());
+  }
 
-module.exports = app;
+  routes() {}
+}
+
+export default new App().app;
+
+// const express = require("express");
+// const app = express();
+// const cors = require("cors");
+// const router = require("./src/routes/router");
+// const database = require("./database");
+
+// database();
+
+// app.use(express.urlencoded({ extended: false }));
+// app.use(express.json());
+// app.use(cors());
+// app.use("/api", router);
+
+// module.exports = app;
